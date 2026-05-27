b5838af4ecd05f5f71e6c3b7b1e5481f48a3c94e
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2026-05-27 09:55:06 +0200

    test set and added junit dependency

---
+++ group6/src/test/java/db/DatabaseResourceTest.java
@@ -0,0 +1,26 @@
+package db;
+
+import group6.java.group6.controllers.TrackDialogController;
+import group6.java.group6.db.DatabaseResource;
+import group6.java.group6.models.Track;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class DatabaseResourceTest {
+    //get the instance of the resource
+    DatabaseResource resource = DatabaseResource.getInstance();
+
+    @Test
+    public void getConnectionTest()
+    {
+        Connection sqlConnection = resource.getSqlConnection();
+        assertNotNull(sqlConnection);
+
+    }
+
+
+}
