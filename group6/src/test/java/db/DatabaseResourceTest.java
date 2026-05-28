e0bb82b0439b6db9cfe2e561eaabe876810b8611
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2026-05-28 10:02:23 +0200

    DatabaseResourceTest class completed

--- group6/src/test/java/db/DatabaseResourceTest.java
+++ group6/src/test/java/db/DatabaseResourceTest.java
@@ -6,9 +6,9 @@ import group6.java.group6.models.Track;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseResourceTest {
     //get the instance of the resource
@@ -19,6 +19,41 @@ public class DatabaseResourceTest {
     {
         Connection sqlConnection = resource.getSqlConnection();
         assertNotNull(sqlConnection);
+    }
+
+/*Verify that requesting another instance means getting the same one was requested before*/
+    @Test
+    public void singletonTest() {
+        DatabaseResource resource2 = DatabaseResource.getInstance();
+        assertSame(resource, resource2);
+    }
+
+
+/*Assert connection is stable within 2 seconds from calling the method getSqlConnection*/
+    @Test
+    public void connectionIsValidTest() throws SQLException {
+        Connection sqlConnection = resource.getSqlConnection();
+        try {
+            assertTrue(sqlConnection.isValid(2));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+/*Assert schema is correctly initialized after requesting the connection to the db through getSqlConnection on resource db */
+    @Test
+    public void schemaInitializedTest() throws SQLException {
+        Connection conn = resource.getSqlConnection();
+        var meta = conn.getMetaData();
+
+        var playlists = meta.getTables(null, null, "playlist", null);
+        assertTrue(playlists.next(), "Playlists table not found");
+
+        var playlistTrack = meta.getTables(null, null, "playlist_track", null);
+        assertTrue(playlistTrack.next(), "Playlist_track table not found");
+
+        var track = meta.getTables(null, null, "track", null);
+        assertTrue(track.next(), "Track table not found");
     }
 
 
