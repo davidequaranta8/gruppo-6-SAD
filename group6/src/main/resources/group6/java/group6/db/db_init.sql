4f27b8dccd85b137d5efcdac2955e2e4a56e6b22
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2026-05-28 16:10:24 +0200

    final fix db schema

--- group6/src/main/resources/group6/java/group6/db/db_init.sql
+++ group6/src/main/resources/group6/java/group6/db/db_init.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS Playlist (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    count_played INTEGER DEFAULT 0
+    count_played INTEGER DEFAULT 0,
     --Constraint to prevent duplicate titles of playlists
     CONSTRAINT uq_playlist_name UNIQUE (title)
 );
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS Track (
     genre VARCHAR(100),
     year_of_publication INTEGER,
     length INTEGER, -- in seconds
-    count_played INTEGER DEFAULT 0
+    count_played INTEGER DEFAULT 0,
     --Constraint to avoid having in track table a duplicate record with same title and author , meaning we have the same
     --track added twice
     CONSTRAINT uq_track_title_artist UNIQUE (title, author)
@@ -31,6 +31,5 @@ CREATE TABLE IF NOT EXISTS Playlist_Track (
     PRIMARY KEY (playlist_id, track_id),
     FOREIGN KEY (playlist_id) REFERENCES Playlist(id) ON DELETE CASCADE,
     FOREIGN KEY (track_id) REFERENCES Track(id) ON DELETE CASCADE
-
 );
 
