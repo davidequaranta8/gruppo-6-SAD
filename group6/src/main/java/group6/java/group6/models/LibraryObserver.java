befbe5e36bded76c306c908c1958ce4393444845
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2026-05-28 15:54:07 +0200

    comment added

--- group6/src/main/java/group6/java/group6/models/LibraryObserver.java
+++ group6/src/main/java/group6/java/group6/models/LibraryObserver.java
@@ -6,6 +6,9 @@ package group6.java.group6.models;
 
  */
 public interface LibraryObserver { // interfaccia Observer
+
+    /*update of concrete observer in observer pattern*/
+
     void onLibraryChanged(); // metodo update del pattern Observer
     // Questo metodo verrà chiamato ogni volta che la libreria subisce una modifica
 }
