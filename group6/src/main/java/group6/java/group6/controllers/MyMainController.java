15f65337b1f100edc0f9659545d89ff13ef6308d
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2026-06-02 12:29:35 +0200

    fix

--- group6/src/main/java/group6/java/group6/controllers/MyMainController.java
+++ group6/src/main/java/group6/java/group6/controllers/MyMainController.java
@@ -167,7 +167,7 @@ public class MyMainController implements LibraryObserver{
 
             // 2. Aggiungiamo la traccia al Singleton
             // Questo farà scattare automaticamente l'Observer e aggiornerà la tabella
-            //ConcreteLibrary.getInstance().addTrack(newTrack); //chiama internamente il trackDao che salva nel db e costruisce il filepath della track
+            ConcreteLibrary.getInstance().addTrack(newTrack); //chiama internamente il trackDao che salva nel db e costruisce il filepath della track
             //prendiamoci il file selezionato nel dialog e
 
             File selectedFile = controller.getSelectedFile();
