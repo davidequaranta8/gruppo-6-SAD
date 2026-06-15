4a99a7d708cbad348dfe9991b678e2025e4f5db0
Umberto1926 <u.scassillo1@studenti.unisa.it>
2026-06-15 18:19:41 +0200

    fix loop

--- group6/src/main/java/group6/java/group6/player/LoopStrategy.java
+++ group6/src/main/java/group6/java/group6/player/LoopStrategy.java
@@ -6,20 +6,18 @@ import java.util.List;
 import group6.java.group6.models.Track;
 
 public class LoopStrategy implements PlaybackStrategy {
-     @Override
-    public Track nextTrack(List<Track> queue, Track current) {
-        int i = queue.indexOf(current);
-        if (i < 0) return null;
-        return queue.get((i + 1) % queue.size());
-    }
-    @Override
-    public Track prevTrack(List<Track> queue, Track current) {
-        int i = queue.indexOf(current);
-        if (i < 0) return null;
-        return queue.get((i - 1 + queue.size()) % queue.size());
-    }
-    @Override
-    public List<Track> buildQueue(List<Track> tracks, Track current) {
-        return new ArrayList<>(tracks);
-    }
+   @Override
+   public Track nextTrack(List<Track> queue, Track current) {
+      return current;
+   }
+
+   @Override
+   public Track prevTrack(List<Track> queue, Track current) {
+      return current;
+   }
+
+   @Override
+   public List<Track> buildQueue(List<Track> tracks, Track current) {
+      return new ArrayList<>(tracks);
+   }
 }
